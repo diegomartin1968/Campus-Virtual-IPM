40f838352af75698d6d19e66628927be145fd850
diegomartin1968 <diegoangelmartin@gmail.com>
2022-05-28 21:00:15 -0300

    21.00

--- scripts/validarIngreso.js
+++ scripts/validarIngreso.js
@@ -1,3 +1,18 @@
-if (ingreso.usuario=="") {
-    alert ("Ingresar nombre")
+document.addEventListener("DOMContentLoaded", function() {
+document.getElementById("ingreso").addEventListener('submit', validacion); 
+  });
+  
+function validacion(submit) {
+    submit.preventDefault();
+    var usuario=document.getElementById("usuario").value;
+    if (usuario == null || usuario.length == 0 || /^\s+$/.test(usuario)) {
+        alert ("ERROR Ingresar Nombre de Usuario");
+        return;
+    }
+    var clave=document.getElementById("clave").value;
+    if (clave == null || clave.length == 0 || /^\s+$/.test(clave) || clave.length<6) {
+        alert (ERROR Ingresar Clave de Acceso válida);
+        return;
+    }
+    this.submit
 }
